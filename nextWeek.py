3f7c8b808bf1dd662c00b5d5d05af2a9937f0250
bgihe <ccs9214221@gmail.com>
2021-04-20 14:00:50 +0800

    auto update

---
+++ nextWeek.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
+from git import Repo
+import os
+import json
+
+def writeOilDict(writeDict):
+    with open("/Users/steven/oiloiloil/upDown.json", "w+") as output:
+        output.write(str(writeDict))
+
+def pushJson():
+    dirfile = os.path.abspath('/Users/steven/oiloiloil') # code的文件位置，我默认将其存放在根目录下
+    repo = Repo(dirfile)
+    g = repo.git
+    g.push()
+    g.add("--all")
+    g.commit("-m auto update")
+    g.push()
+    print("Successful push!")
+
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+url = 'https://www.cpc.com.tw/'
+driver.get(url)
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+print(soup.find("b", class_="rate").i.string)
+print(soup.find("b", class_="sys").string)
+
+oilDict = {}
+oilDict['rate'] = soup.find("b", class_="rate").i.string
+oilDict['sys'] = soup.find("b", class_="sys").string
+writeOilDict(json.dumps(oilDict, indent=4))
+
+driver.close()
+pushJson()
